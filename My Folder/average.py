20ba50fabf412038c60c1834f96e200c2eb1ed0f
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-12 17:35:38 +0530

    Commit 2

--- My Folder/average.py
+++ My Folder/average.py
@@ -1,6 +1,10 @@
+# Define the function that creates the average
 def ave (num1, num2):
     avg = num1 + num2 / 2
 
+# Ask the input from the user
 num1 = float(input("\nEnter the first number: "))
 num2 = float(input("Enter the second number: "))
+
+# Print the Average
 print("\nThe average of ", num1, "and", num2, "is", ave(num1, num2)) 
