30a69b3db53d09f6cc580d4ba8111ac45ff3cace
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-12 17:20:00 +0530

    Commit 1

---
+++ My Folder/average.py
@@ -0,0 +1,5 @@
+num1 = float(input("\nEnter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum = num1 + num2
+avg = sum / 2
+print("\nThe average of ", num1, "and", num2, "is", avg) 
