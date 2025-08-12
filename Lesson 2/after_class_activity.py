d7cd4a5acaa2774db56084fdfcdbe5a265331e5a
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-12 17:02:55 +0530

    Commit 1

---
+++ Lesson 2/after_class_activity.py
@@ -0,0 +1,11 @@
+suhas = "26 July"
+charl = "14 January"
+ankit = "7 December"
+heily = "23 September"
+julie = "4 June"
+
+print("The birthday of Suhas is", suhas)
+print("The birthday of Charl is", charl)
+print("The birthday of Ankit is", ankit)
+print("The birthday of Heily is", heily)
+print("The birthday of Julie is", julie)
