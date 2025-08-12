abf8098edc478cec5238e50a5be1a07b495c662e
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-12 17:33:37 +0530

    Commit 2

--- My Folder/average.py
+++ My Folder/average.py
@@ -1,5 +1,6 @@
+def ave (num1, num2):
+    avg = num1 + num2 / 2
+
 num1 = float(input("\nEnter the first number: "))
 num2 = float(input("Enter the second number: "))
-sum = num1 + num2
-avg = sum / 2
-print("\nThe average of ", num1, "and", num2, "is", avg) 
+print("\nThe average of ", num1, "and", num2, "is", ave(num1, num2)) 
