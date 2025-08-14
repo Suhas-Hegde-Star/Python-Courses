4108d7a2e106a00fde20d010f37a75d70003f0a9
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-14 17:04:42 +0530

    Commit 1

---
+++ My Folder/areac.py
@@ -0,0 +1,8 @@
+import math
+
+def circle(rad, area):
+    rad = float(input("Enter the Radius of the circle"))
+    area = math.pow(rad, 2) * math.pi
+    print("The area of the circle with", rad, "radius is", area)
+
+circle(rad= any, area= any)
