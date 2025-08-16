d95bb20c6f30e955a436dee5d33014abeea1b1fd
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-16 12:02:42 +0530

    Commit 1

---
+++ My Folder/Calculaters/calc.py
@@ -0,0 +1,12 @@
+import math
+import sys
+
+def square(num, sq):
+    num = int(input("Enter the number"))
+    sq = num ** 2
+    print("Tne square of", num, "is", sq)
+
+def root(num, rot):
+    num = int(input("Enter the number"))
+    rot = math.sqrt(num)
+    print("Tne square of", num, "is", rot)
