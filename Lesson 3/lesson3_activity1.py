2657a7dd5aca95c9281ebf747a9480cba4a01ec4
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-16 14:28:18 +0530

    Commit 1

---
+++ Lesson 3/lesson3_activity1.py
@@ -0,0 +1,11 @@
+a = 2
+print("The type of", a, "is", type(a))
+
+a = 6.4
+print("The type of", a, "is", type(a))
+
+a = "Hello"
+print("The type of", a, "is", type(a))
+
+a = True
+print("The type of", a, "is", type(a))
