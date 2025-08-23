373a7e5adfc217c314914fb02d05f7204acca5d5
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-23 14:38:45 +0530

    Background Commit

---
+++ Lesson 4/lesson4_activity2.py
@@ -0,0 +1,19 @@
+amount = int(input("Enter amount: "))
+
+n500 = amount // 500
+n200 = (amount % 500) // 200
+n100 = ((amount % 500) % 200) // 100
+n50 = (amount % 100) // 50
+n20 = (amount % 20) % 50 // 20
+n10 = (amount % 50) // 10
+n5 = (amount % 10) // 5
+n1 = (amount % 5) // 1
+
+print ("500 note: ", n500)
+print ("200 note: ", n200)
+print ("100 note: ", n100)
+print ("50 note: ", n50)
+print ("20 note: ", n20)
+print ("10 coin: ", n10)
+print ("5 coin: ", n5)
+print ("1 coin: ", n1)
