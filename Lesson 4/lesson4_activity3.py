1b0b3a151c34fe657eef575642aec8ca0daff7a4
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-23 14:59:36 +0530

    Change C 1

--- Lesson 4/lesson4_activity3.py
+++ Lesson 4/lesson4_activity3.py
@@ -10,10 +10,10 @@ sum = eng + bio + che + phy + mat + la2 + la3
 avg = (sum) / 7
 percent = (sum / 700) * 100
 
-if eng or bio or che or phy or mat or la2 or la3 > 100:
+if eng > 100 or bio  > 100 or che  > 100 or phy  > 100 or mat > 100  or la2 > 100 or la3 > 100:
     print("invalid input")
-elif eng or bio or che or phy or mat or la2 or la3 < 0:
-    print("invalid input")
+elif eng < 0 or bio  < 0 or che  < 0 or phy  < 0 or mat <0  or la2 < 0 or la3 < 0:
+    print("invalid input entered")
 else:
     print("Your average marks is", avg)
     print("Your totsl marks is", sum)
