ea6ee51a3d3392965d4bed9737fad61862f75804
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-23 14:21:18 +0530

    Background Commit

---
+++ Lesson 4/lesson4_activity1.py
@@ -0,0 +1,11 @@
+val1 = 45
+val2 = 84
+val3 = 34
+val4 = 75
+val5 = 85
+
+sum = val1 + val2 + val3 + val5 + val4
+avg = sum / 5
+
+print ("sum of the numbers is", sum)
+print("The average of the numbers is", avg)
