b7d344fc8714b3279570c40f2dcadaa202c46ba7
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-24 11:21:28 +0530

    Improve 2

---
+++ My Folder/Calculaters/asker.py
@@ -0,0 +1,42 @@
+import sys
+import a_a
+import p2
+import nums
+import Exit
+
+def ask(wanted):
+    while True:
+        wanted = input("Enter what you want(Basic Words)\n")
+        if wanted.lower() == "square":
+            a_a.square(num= any, sq= any)
+        elif wanted.lower() == Exit.exit[1] or Exit.exit[2] or Exit.exit[3]:
+            print("Bye\nSee you next time")
+            sys.exit
+        elif wanted.lower() == "square root":
+            a_a.root(num= any, rot= any)
+        elif wanted.lower() == "exponentation":
+            a_a.exponentation(num= any, ra= any, ans= any)
+        elif wanted.lower() == "cube":
+            a_a.cube(num= any, cu= any)
+        elif wanted.lower() == "cube root":
+            a_a.cube_root(num= any, cr= any)
+        elif wanted.lower() == "area of circle":
+            a_a.circle(rad= any, area= any)
+        elif wanted.lower() == "area of square":
+            a_a.square(side= any, area= any)
+        elif wanted.lower() == "area of rectangle":
+            p2.rect(length= any, breadth= any, area= any)
+        elif wanted.lower() == "area of triangle":
+            p2.tri(o= any, t=any, th= any, waa= any, area= any, s= any)
+        elif wanted.lower() == "addition" or "add":
+            p2.add(n1= any, n2= any)
+        elif wanted.lower() == "subtraction" or "subtract":
+            p2.sub(n1= any, n2= any)
+        elif wanted.lower() == "multiplication" or "multiply":
+            p2.mul(n1= any, n2= any)
+        elif wanted.lower() == "division" or "divide":
+            p2.div(n1= any, n2= any)
+        elif wanted.lower() == "triangle numbers":
+            nums.tri_num(i= any, want= any, n= any)
+        else:
+            print("Input may be invalid\nOR\nWe do not have the data")
