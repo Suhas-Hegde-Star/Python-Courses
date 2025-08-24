ad2609d2336f5a7ff0afdc52d775dcded3a6452a
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-24 10:38:06 +0530

    Commit 24

--- My Folder/Calculaters/calc.py
+++ My Folder/Calculaters/calc.py
@@ -1,42 +1,6 @@
 import math
 import sys
 
-def square(num, sq):
-    num = int(input("Enter the number "))
-    sq = num ** 2
-    print("The square of", num, "is", sq)
-
-def root(num, rot):
-    num = int(input("Enter the number "))
-    rot = math.sqrt(num)
-    print("The square root of", num, "is", rot)
-
-def cube(num, cu):
-    num = int(input("Enter the number "))
-    cu = num ** 3
-    print("The cube of", num, "is", cu)
-
-def cube_root(num, cr):
-    num = int(input("Enter the number "))
-    cr = math.pow (num, 1/3)
-    print("The cube root of", num, "is", cr)
-
-def exponentation(num, ra, ans):
-    num = int(input("Enter the base "))
-    ra = int(input("Enter the exponent "))
-    ans = num ** ra
-    print("The number", num, "raised to", ra, "is", ans)
-
-def circle(rad, area):
-    rad = float(input("Enter the Radius of the Circle "))
-    area = math.pow(rad, 2) * math.pi
-    print("The area of the circle with", rad, "as the radius is", area)
-
-def square(side, area):
-    side = float(input("Enter the Length of the Square "))
-    area = math.pow(side, 2)
-    print("The area of the square with", side, "as the length is", area)
-
 def rect(length, breadth, area):
     length = float(input("Enter the Length of the Rectangle "))
     breadth = float(input("Enter the Breadth of the Rectangle "))
@@ -89,12 +53,12 @@ def ask(wanted):
     while True:
         wanted = input("Enter what you want(Basic Words)\n")
         if wanted.lower() == "square":
-            square(num= any, sq= any)
+            file1.square(num= any, sq= any)
         elif wanted.lower() == "exit":
             print("Bye\nSee you next time")
             sys.exit
         elif wanted.lower() == "square root":
-            root(num= any, rot= any)
+            file1.root(num= any, rot= any)
         elif wanted.lower() == "exponentation":
             exponentation(num= any, ra= any, ans= any)
         elif wanted.lower() == "cube":
