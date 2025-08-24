27c0a0863c08a44cc7139bfddb940b42549146c6
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-24 10:51:50 +0530

    Run Final

--- My Folder/Calculaters/Final_Calculator.py
+++ My Folder/Calculaters/Final_Calculator.py
@@ -1,43 +1,42 @@
-import math
 import sys
-import file1
-import file2
-import file3
+import a_a
+import p2
+import nums
 
 def ask(wanted):
     while True:
         wanted = input("Enter what you want(Basic Words)\n")
         if wanted.lower() == "square":
-            file1.square(num= any, sq= any)
+            a_a.square(num= any, sq= any)
         elif wanted.lower() == "exit":
             print("Bye\nSee you next time")
             sys.exit
         elif wanted.lower() == "square root":
-            file1.root(num= any, rot= any)
+            a_a.root(num= any, rot= any)
         elif wanted.lower() == "exponentation":
-            file1.exponentation(num= any, ra= any, ans= any)
+            a_a.exponentation(num= any, ra= any, ans= any)
         elif wanted.lower() == "cube":
-            file1.cube(num= any, cu= any)
+            a_a.cube(num= any, cu= any)
         elif wanted.lower() == "cube root":
-            file1.cube_root(num= any, cr= any)
+            a_a.cube_root(num= any, cr= any)
         elif wanted.lower() == "area of circle":
-            file1.circle(rad= any, area= any)
+            a_a.circle(rad= any, area= any)
         elif wanted.lower() == "area of square":
-            file1.square(side= any, area= any)
+            a_a.square(side= any, area= any)
         elif wanted.lower() == "area of rectangle":
-            file2.rect(length= any, breadth= any, area= any)
+            p2.rect(length= any, breadth= any, area= any)
         elif wanted.lower() == "area of triangle":
-            file2.tri(o= any, t=any, th= any, waa= any, area= any, s= any)
+            p2.tri(o= any, t=any, th= any, waa= any, area= any, s= any)
         elif wanted.lower() == "addition" or "add":
-            file2.add(n1= any, n2= any)
+            p2.add(n1= any, n2= any)
         elif wanted.lower() == "subtraction" or "subtract":
-            file2.sub(n1= any, n2= any)
+            p2.sub(n1= any, n2= any)
         elif wanted.lower() == "multiplication" or "multiply":
-            file2.mul(n1= any, n2= any)
+            p2.mul(n1= any, n2= any)
         elif wanted.lower() == "division" or "divide":
-            file2.div(n1= any, n2= any)
+            p2.div(n1= any, n2= any)
         elif wanted.lower() == "triangle numbers":
-            file3.tri_num(i= any, want= any, n= any)
+            nums.tri_num(i= any, want= any, n= any)
         else:
             print("Input may be invalid\nOR\nWe do not have the data")
 
