d45ca11cad6e4cbebb5ee88a288bd4843adace80
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-25 15:37:44 +0530

    Connit

--- My Folder/Calculaters/average.py
+++ My Folder/Calculaters/average.py
@@ -1,18 +1,14 @@
-// h = int(input ("Enter number")) 
-// sum = 0
+"""
 
-// for i in range (0, h):
-//    i = int(input ("enter num") )
-  //  sum = sum + i
-//
-//print (sum)
+h = int(input ("Enter number")) 
+sum = 0
 
-using System;
+for i in range (0, h):
+    i = int(input ("enter num") )
+    sum = sum + i
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+print (sum)
+
+"""
+
+print("😊")
