72d5cca19c792fc2e66d9ca14580795adc85d25c
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-30 14:22:29 +0530

    B C

---
+++ Lesson 5/lesson_activity4.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+mum = int(input("Enter another number: "))
+
+if num > mum:
+    print(num, "is greater than", mum)
+elif num < mum:
+    print(num, "is less than", mum)
+else:
+    print(num, "is equal to", mum)
