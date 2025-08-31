06866b8e5c90a0680d63fc474581dbcff0703e85
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-08-31 16:45:24 +0530

    Commit i

---
+++ Lesson 6/lesson6_activity3.py
@@ -0,0 +1,20 @@
+"""
+
+weight/(height/100)**2
+
+"""
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number: "))
+
+BMI = a / (b / 100) ** 2
+
+if BMI < 18.5:
+    print("Underweight")
+elif BMI <= 24.9:
+    print("Normal weight")
+elif BMI <= 29.9:
+    print("Overweight")
+elif BMI <= 34.9:
+    print("Obesity")
+else:
+    print("Severe obesity")
