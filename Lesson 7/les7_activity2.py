b82d49aff3a7cbd0db3458cf3b79d800f5856314
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-09-06 14:31:05 +0530

    Commmit

---
+++ Lesson 7/les7_activity2.py
@@ -0,0 +1,11 @@
+a = int(input("Enter an integer: "))
+b = int(input("Enter another integer: "))
+
+print("a<<1:", a << 1)
+print("b<<1:", b << 1)
+
+a = int(input("Enter an integer: "))
+b = int(input("Enter another integer: "))
+
+print("a>>1:", a >> 1)
+print("b>>1:", b >> 1)
