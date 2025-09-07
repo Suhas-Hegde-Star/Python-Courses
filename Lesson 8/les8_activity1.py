538479b5c920f9f8e11a9d4fd09e656261db6d43
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-09-07 16:26:48 +0530

    Mod 1 Act 1

---
+++ Lesson 8/les8_activity1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = int(input("Enter one more number: "))
+d = int(input("Enter the last number: "))
+e = (((a + b) + (c - d)) * ((a + d) * (b + c)))
+
+print ("The result of ((a + b) + (c - d)) * ((a + d) * (b + c)) is:", e)
