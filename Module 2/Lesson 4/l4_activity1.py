cb7498c3bb8721603a8f6c5a6ea23fe3ca234ba7
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-09-21 16:44:06 +0530

    Counted Characters Here

--- Module 2/Lesson 4/l4_activity1.py
+++ Module 2/Lesson 4/l4_activity1.py
@@ -1,12 +1,15 @@
 count = 0
-i = 0
+start = 0 
 
 string = input("Enter sentence: ")
 charac = input("Enter a letter: ")
+letter = len(charac)
+end = letter
 
-while i < len(string):
-    if string[i] == charac:
+while end < len(string):
+    if string[start:end] == charac:
         count += 1
-    i += 1
+    end += 1
+    start += 1
 
-print("The amount of times ihe letter \"" + charac + "\" occours in \"" + string + "\" is", count)
+print("The amount of times ihe letter \"" + charac + "\" occours in \n\"" + string + "\n\" is", count)
