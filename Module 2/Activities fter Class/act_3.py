b64754d5a76b7a79cad0720d60214a28ae22a0d1
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-09-22 15:29:46 +0530

    exponents calculation

---
+++ Module 2/Activities fter Class/act_3.py
@@ -0,0 +1,7 @@
+def exponentation(num, ra, ans):
+    num = int(input("Enter the base "))
+    ra = int(input("Enter the exponent "))
+    ans = num ** ra
+    print("The number", num, "raised to", ra, "is", ans)
+
+exponentation(num= any, ra= any, ans= any)
