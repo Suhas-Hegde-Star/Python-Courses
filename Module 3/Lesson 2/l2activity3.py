85cef79908c2111aa9af1cdfeeb549c3cc0eb747
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-10-05 16:56:44 +0530

    Factoials of 2.0 Version of

--- Module 3/Lesson 2/l2activity3.py
+++ Module 3/Lesson 2/l2activity3.py
@@ -1,12 +1,15 @@
 def factorial(x):
-    """\
-This Function is used to find the Factorial of a particular number.\
+    """
+    This Function is used to find the Factorial of a particular number.
     """
     if x == 1 or 0:
         return 1
     else:
         return x * factorial(x-1)
     
+inp = int(input("Enter a Number: "))
 print(factorial.__doc__)
-print(factorial(10))
-print(factorial(5))
+
+while inp != 0:
+    print(factorial(inp))
+    inp -= 1
