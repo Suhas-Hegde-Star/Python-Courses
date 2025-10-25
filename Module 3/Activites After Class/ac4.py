dcf05299adc4ae743e36cefbc80e68dc8ba8fe58
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-10-25 14:59:41 +0530

    Activites **3 AmAM

--- Module 3/Activites After Class/ac4.py
+++ Module 3/Activites After Class/ac4.py
@@ -1,16 +1,6 @@
-import datetime
-
-now = datetime.datetime.now().strftime("Date = (%Y-%m-%d) :: :: Time = (%H:%M:%S)")
-log = []
-
 try:
-    user_input = input("Enter your age (or type 'clear,log' to clear the log): ")
-    if user_input.lower() == "clear,log":
-        with open("log3.txt", "w") as f:
-            f.write("")
-        print("Log file cleared.")
-    num1, num2 = map(float, user_input.split(","))
-    result, resul = int(num1) and int(num2)
-    print("The result of division is:", result)
-    log.append(f"{now} :: Division Successful\n")
-except S:
+    sss = int(input("Enter your age: "))
+except ValueError:
+    print("Invalid input. Please enter a valid integer for age.")
+
+print("You entered age:", sss)
---
+++ Module 3/Activites After Class/ac5.py
@@ -0,0 +1,9 @@
+import math
+
+sinsin = (math.sin(int(input("Enter a number to find its sine: "))))
+coscos = (math.cos(int(input("Enter a number to find its cosine: "))))
+tantan = (math.tan(int(input("Enter a number to find its tangent: "))))
+
+print("Sine value is:", sinsin)
+print("Cosine value is:", coscos)
+print("Tangent value is:", tantan)
