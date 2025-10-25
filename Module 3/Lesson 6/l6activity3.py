2a2217cf997b560613e709078d221844cbe68479
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-10-25 14:39:56 +0530

    Information And Maths

---
+++ Module 3/Lesson 6/l6activity3.py
@@ -0,0 +1,13 @@
+def information():
+    print("This is Information.")
+    print("This is my code.")
+
+information()
+information()
+information()
+information()
+
+def add(a, b):
+    return a + b
+
+print(add(1, 2))
