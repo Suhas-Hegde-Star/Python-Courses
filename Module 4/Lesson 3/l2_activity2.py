c540646b3e4b4b75bb734125028c7c88e528588d
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-11-22 14:49:05 +0530

    Start

---
+++ Module 4/Lesson 3/l2_activity2.py
@@ -0,0 +1,18 @@
+students = {
+    "id1" : {"Name": "Alice",   "Subject": "Computer Science", "Grade": "VII"},
+    "id2" : {"Name": "Bob",     "Subject": "Mathematics",      "Grade": "VIII"},
+    "id3" : {"Name": "Charlie", "Subject": "Physics",          "Grade": "VI"},
+    "id4" : {"Name": "Diana",   "Subject": "Chemistry",        "Grade": "VII"},
+    "id5" : {"Name": "Ethan",   "Subject": "Biology",          "Grade": "VIII"},
+    "id6" : {"Name": "Alice",   "Subject": "Computer Science", "Grade": "VII"},
+}
+
+result = {}
+
+for key, value in students.items():
+    name = value["Name"]
+    if name not in result:
+        result[name] = value
+
+for i in result.values():
+    print(i)
