2724903ad9343c9411c9cdc027732643b4572dbb
madhuhegde84 <madhuhl@gmail.com>
2025-11-27 19:42:01 +0530

    Commit 159

--- Module 4/Lesson 3/l2_activity2.py
+++ Module 4/Lesson 3/l2_activity2.py
@@ -3,7 +3,7 @@ students = {
     "id2" : {"Name": "Bob",     "Subject": "Mathematics",      "Grade": "VIII"},
     "id3" : {"Name": "Charlie", "Subject": "Physics",          "Grade": "VI"},
     "id4" : {"Name": "Diana",   "Subject": "Chemistry",        "Grade": "VII"},
-    "id5" : {"Name": "Ethan",   "Subject": "Biology",          "Grade": "VIII"},
+    "id5" : {"Name": "Ethan",   "Subject": "Biology",          "Grade": "IX"},
     "id6" : {"Name": "Alice",   "Subject": "Computer Science", "Grade": "VII"},
 }
 
