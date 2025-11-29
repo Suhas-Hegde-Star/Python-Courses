ec2580881f7feb4203877ee3a76adc52d8745267
madhuhegde84 <madhuhl@gmail.com>
2025-11-29 14:41:26 +0530

    Commit 162

---
+++ Module 4/Lesson 4/l4_activity3.py
@@ -0,0 +1,11 @@
+# Set the Set value of the Set
+set1 = {"Hello", "World","Can", "Be", "Written", "Python", "is", "my"}
+set2 = {"Python", "is", "my", "Java", "C++", "World"}
+
+# Print the Set value of the Set
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+# Intersection Of Sets
+set3 = set1.intersection(set2)
+print("Intersection :", set3)
