557fc559470ed878b16b2811d5a4734a30d8d1a9
madhuhegde84 <madhuhl@gmail.com>
2025-12-14 16:46:52 +0530

    Commit 171

---
+++ Module 4/Lesson 6/l6_activity1.py
@@ -0,0 +1,56 @@
+board = {
+    1: " ",
+    2: " ",
+    3: " ",
+    4: " ",
+    5: " ",
+    6: " ",
+    7: " ",
+    8: " ",
+    9: " "
+}
+
+board_keys = []
+for key in board:
+    board_keys.append(key)
+
+def print_board(board):
+    print(board[1] + "  | " + board[2] + " | " + board[3])
+    print("---+---+---")
+    print(board[4] + "  | " + board[5] + " | " + board[6])
+    print("---+---+---")
+    print(board[7] + "  | " + board[8] + " | " + board[9])
+print_board(board)
+
+def game():
+    turn = "X"
+    for i in range(10):
+        print("Turn for " + turn + ". Move on which space?")
+        move = int(input())
+        if board[move] == " ":
+            board[move] = turn
+            print_board(board)
+            if turn == "X":
+                turn = "O"
+            else:
+                turn = "X"
+        else:
+            print("That space is already occupied. Try again.")
+            continue
+
+    if i >= 5:
+        if  (board[1] == board[2] == board[3] != " ") or \
+            (board[4] == board[5] == board[6] != " ") or \
+            (board[7] == board[8] == board[9] != " ") or \
+            (board[1] == board[4] == board[7] != " ") or \
+            (board[2] == board[5] == board[8] != " ") or \
+            (board[3] == board[6] == board[9] != " ") or \
+            (board[1] == board[5] == board[9] != " ") or \
+            (board[3] == board[5] == board[7] != " "):
+                print(" **** Game Over. " + turn + " wins! ****")
+                break
+        
+    if i == 9:
+        print(" **** Game Over. It's a Tie! ****")
+
+game()
