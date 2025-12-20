eb14a4265002c558ea1afe4355465595ff727c33
madhuhegde84 <madhuhl@gmail.com>
2025-12-20 15:16:30 +0530

    Commit 176

---
+++ Module 5/After Class Activitiy/aa1.py
@@ -0,0 +1,12 @@
+import random
+
+class doggy():
+    def __init__(self, name, breed, color):
+        self.name = name
+        self.breed = breed
+        self.color = color
+
+boggy_doggy = doggy("Boggy", "Bulldog", "Brown")
+print(boggy_doggy.name)
+poocchhiiee_doody = doggy("Poocchhiiee", "Poodle", "White")
+print(poocchhiiee_doody.breed)
