f05d20da9e636f35ff9ef4f7088e48733c4f0541
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2026-01-17 15:04:25 +0530

    Commit 202

---
+++ Module 6/Lesson 1/1a2.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+
+height = 1025
+widthh = 1024
+
+screen = pygame.display.set_mode((widthh, height))
+
+display_surface = pygame.Surface((widthh, height))
+pygame.display.set_caption("Virat Kholi with his Lamborghini")
+
+background = pygame.transform.scale(pygame.image.load("C:\\Users\\madhu\\OneDrive\\Documents\\Python Courses\\Module 6\\Lesson 1\\Kholi.jpg").convert(), (widthh, height))
+
+images2 = pygame.transform.scale(pygame.image.load("C:\\Users\\madhu\\OneDrive\\Documents\\Python Courses\\Module 6\\Lesson 1\\Lamborghini.jpg").convert(), (275, 183))
+
+images2_rect = images2.get_rect()
+images2_rect.center = (widthh // 2, height // 2)
+
+text = pygame.font.Font(None, 32).render("Virat Kholi with his Lamborghini", True, (255, 255, 255))
+text_rect = text.get_rect(center=(widthh // 2, 30))
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    display_surface.blit(background, (0, 0))
+    display_surface.blit(images2, images2_rect)
+    display_surface.blit(text, text_rect)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
