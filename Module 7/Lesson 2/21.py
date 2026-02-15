ae6534317c73b0706a900d2f371eb0a214eedbda
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2026-02-15 16:41:59 +0530

    Commit 219

---
+++ Module 7/Lesson 2/21.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+r = Tk()
+r.title("Number Pad")
+r.geometry("400x400")
+number = [    [9, 8, 7],     [6, 5, 4],     [3, 2, 1],     ["#", 0, "><"]    ]
+
+for i in range(4):
+    r.rowconfigure(i, weight=1, minsize=50)
+    for j in range(3):
+        r.columnconfigure(j, weight=1, minsize=50)
+        frame = Frame(master=r, relief=SOLID, borderwidth=6.7)
+        frame.grid(row=i, column=j, padx=5, pady=5)
+        label = Label(master=frame, text=number[i][j])
+        label.pack( padx=5, pady=5)
+
+r.mainloop()
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # I go to Python Classes. Hence This Repository
-# He likes Doing *6-7* and *6-7*
-# He is a Humongous QWERTYUIOP and qwertyuiop Fan
-# # # # # # # # #
